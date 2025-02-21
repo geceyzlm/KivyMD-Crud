105aa8ca184af9db15b06a31089876b49a1e4e17
geceyzlm <usamealperengece27@gmail.com>
2025-02-21 16:42:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen, ScreenManager
+
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton,MDRectangleFlatButton
 from kivy.metrics import dp
@@ -8,38 +9,41 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivymd.app import MDApp
 from kivymd.uix.datatables import MDDataTable
 
-class Tablo(MDApp):
-    dialog=None#burada olucak 
+
+class Table(MDApp):
+    dialog=None
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.data_tables = None
     
-    global contacts
-    contacts = []
+        global contacts
+        contacts = []
+    
 
     def build(self):
-        global ekran
-        ekran=ScreenManager()
-        ekran.add_widget(Builder.load_file("maug.kv"))
-        return ekran
-    
-    
+        global sc
+        sc=ScreenManager()
+        sc.add_widget(Builder.load_file("main.kv")) #1
+        sc.add_widget(Builder.load_file("update.kv")) #2
+        sc.add_widget(Builder.load_file("add.kv")) #3
+        return sc
+
     def add_datatable(self):
         import sqlite3
-        vt = sqlite3.connect('son.db')
+        vt = sqlite3.connect('zl.db')
         im = vt.cursor()
-        im.execute("select * from COMPANY")
-        veri=im.fetchall()
+        im.execute("select * from person") #person table name
+        data=im.fetchall()
         self.data_tables = MDDataTable(
-            size_hint=(0.5, 0.5),
+            size_hint=(0.75, 0.5),
             use_pagination=True,
             check=True,
             column_data=[
                 ("id",dp(30)),
-                ("ad", dp(30)),
-                ("yaş", dp(30)),
-                ("soyad", dp(30)),
-                ("puan", dp(30))
+                ("name", dp(30)),
+                ("age", dp(30)),
+                ("city", dp(30))
+               
             ],
             row_data=[
                 (
@@ -47,109 +51,107 @@ class Tablo(MDApp):
                 i[:][1],
                 i[:][2],
                 i[:][3],   
-                i[:][4],
+                
                 )
-                for i in veri  
+                for i in data 
             ],
 
 
         )
         self.data_tables.bind(on_check_press=self.on_check_press) 
-        ekran.get_screen("you").ids.data.add_widget( self.data_tables)
-       
+        sc.get_screen("page").ids.datatable.add_widget( self.data_tables)    
 
-    def on_start(self):#başlangıçta
-        self.add_datatable()
-   
+    def on_start(self):
+        self.add_datatable() 
+
     def on_check_press(self, instance_table,current_row):
-        if current_row[0] not in contacts:
-            contacts.append(current_row[0])
-        else:
-            contacts.remove(current_row[0])
+            if current_row[0] not in contacts:
+                contacts.append(current_row[0])
+            else:
+                contacts.remove(current_row[0])
 
-        print(contacts)
+            print(contacts)
 
-    def delete(self):
-        if not self.dialog:
-            self.dialog=MDDialog(
-                text="SİLME İŞLEMİNDEN EMİNMİSİNİZ",
+     #delete data
+    def delete(self):  
+            #alert(message box)
+           self.dialog=MDDialog(
+                text="are you sure",
                 buttons=[
-                    MDFlatButton(text="[color=3338FF]iptal[/color]",on_release=self.close),
-                    MDRectangleFlatButton(text="[color=096C7F]kabul[/color]",on_release=self.open)
+                    MDFlatButton(text="[color=3338FF]no[/color]",on_release=self.close),
+                    MDRectangleFlatButton(text="[color=096C7F]yes[/color]",on_release=self.open)
                 ],
 
             )
-            self.dialog.open()
+           self.dialog.open()
     def close(self,obj):
             self.dialog.dismiss()
     def open(self,obj):     
             for i in contacts:
                 if i:
                     import sqlite3
-                    vt = sqlite3.connect('son.db')
+                    vt = sqlite3.connect('zl.db')
                     im = vt.cursor()
-                    im.execute(f"delete from COMPANY where id={i} " )  
+                    im.execute(f"delete from person where person_id={i} " )  
                     vt.commit()
-                    ekran.get_screen("you").ids.data.remove_widget(self.data_tables)
+                    sc.get_screen("page").ids.datatable.remove_widget(self.data_tables)
                     self.add_datatable()
             self.dialog.dismiss() 
+            contacts.clear() 
 
-    def guncelle(self,password,user):
+    # open update.kv
+    def updatenewpage(self):
         for i in contacts:
                 if i:
                     import sqlite3
-                    vt = sqlite3.connect('son.db')
+                    vt = sqlite3.connect('zl.db')
                     im = vt.cursor()
-                    im.execute(f"update COMPANY  set ad=?,yas=? where id={i}",(user,int(password)))  #int çunku yasın turu o
+                    im.execute("select * from person where person_id=?",(i)) #person table name
+                    data=im.fetchall()
+                    for j in data:
+                        sc.get_screen("upd").ids.name.text=j[1]
+                        sc.get_screen("upd").ids.age.text=j[2]
+                        sc.get_screen("upd").ids.city.text=j[3]
+                       
+        sc.current="upd"
+
+
+
+        #open ad.kv
+    def addnewpage(self):
+        sc.current="add"
+
+    #open main.kv
+    def back(self):
+        sc.current="page" #page nane/screen name
+
+    #update data
+    def update(self,username,age,city):
+        for i in contacts:
+                if i:
+                    import sqlite3
+                    vt = sqlite3.connect('zl.db')
+                    im = vt.cursor()
+                    im.execute("update person set username=?,age=?,city=? where person_id=?",(username,age,city,i))   
                     vt.commit()
-                    ekran.get_screen("you").ids.data.remove_widget(self.data_tables)
-                    self.add_datatable()
+                    sc.get_screen("page").ids.datatable.remove_widget(self.data_tables)
+                    self.add_datatable() 
 
-    def aramax(self,arama):
+        contacts.clear() 
+        sc.current="page"
+
+
+    def add(self,username,age,city):
         import sqlite3
-        vt = sqlite3.connect('son.db')
+        vt = sqlite3.connect('zl.db')
         im = vt.cursor()
-        im.execute(f"select * from COMPANY where ad like '{arama}%'  ")
-        
-        veri=im.fetchall()
-
-        self.arama_tables = MDDataTable(
-            size_hint=(0.5, 0.5),
-            use_pagination=True,
-            check=True,
-            column_data=[
-                ("id",dp(30)),
-                ("ad", dp(30)),
-                ("yaş", dp(30)),
-                ("soyad", dp(30)),
-                ("puan", dp(30))
-            ],
-            row_data=[
-                (
-                i[:][0],
-                i[:][1],
-                i[:][2],
-                i[:][3],   
-                i[:][4],
-                )
-                for i in veri  
-            ],
-
-
-        )
-        ekran.get_screen("you").ids.data.remove_widget(self.data_tables)
-
-        ekran.get_screen("you").ids.data.add_widget(self.arama_tables)   
-
-    def ekleme(self,password,user,soyad,puan): #passwordu yaş diye al
-        import sqlite3
-        vt = sqlite3.connect('son.db')
-        im = vt.cursor()
-        im.execute("insert into COMPANY( ad, yas,soyad, puan)  VALUES(?,?,?,?)",(user,password,soyad,int(puan)))
+        im.execute("insert into person(username,age,city) VALUES(?,?,?)",(username,age,city))
         vt.commit()
 
-        ekran.get_screen("you").ids.data.remove_widget(self.data_tables)
-        self.add_datatable()
+        sc.get_screen("page").ids.datatable.remove_widget(self.data_tables)
+        self.add_datatable() #functıon
 
+        contacts.clear() 
+        sc.current="page"    
 
-Tablo().run()
+Table().run()
